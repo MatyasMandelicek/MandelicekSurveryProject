a2d6460ba86a95728cb3fb154a313010db352ab8
Matyas Mandelicek <93654944+MatyasMandelicek@users.noreply.github.com>
2025-04-02 14:34:57 +0200

    admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const firebaseURL = 'https://console.firebase.google.com/u/0/project/survey-55058/database/survey-55058-default-rtdb/data/~2F'; // Replace with your Firebase URL
+const firebaseURL = 'https://survey-55058-default-rtdb.europe-west1.firebasedatabase.app'; // Replace with your Firebase URL
 
 const validUsername = "admin";
 const validPassword = "admin123";
